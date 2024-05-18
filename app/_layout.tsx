d21be8ee88688c67e4d0a57449dc4b274d825318
Dino Danic <dino.danic@gmail.com>
2024-05-18 14:48:48 +0200

    navigation init

--- app/_layout.tsx
+++ app/_layout.tsx
@@ -1,11 +1,23 @@
-import { DarkTheme, DefaultTheme, ThemeProvider } from '@react-navigation/native';
-import { useFonts } from 'expo-font';
-import { Stack } from 'expo-router';
-import * as SplashScreen from 'expo-splash-screen';
-import { useEffect } from 'react';
-import 'react-native-reanimated';
+import {
+  DarkTheme,
+  DefaultTheme,
+  ThemeProvider,
+} from "@react-navigation/native";
+import { useFonts } from "expo-font";
+import { Tabs } from "expo-router";
+import * as SplashScreen from "expo-splash-screen";
+import { useEffect } from "react";
+import "react-native-reanimated";
 
-import { useColorScheme } from '@/hooks/useColorScheme';
+import { useColorScheme } from "@/hooks/useColorScheme";
+import { SessionProvider } from "@/features/auth/ctx";
+import {
+  HeartIcon,
+  HomeIcon,
+  MessageCircleIcon,
+  SearchIcon,
+  UserIcon,
+} from "lucide-react-native";
 
 // Prevent the splash screen from auto-hiding before asset loading is complete.
 SplashScreen.preventAutoHideAsync();
@@ -13,7 +25,7 @@ SplashScreen.preventAutoHideAsync();
 export default function RootLayout() {
   const colorScheme = useColorScheme();
   const [loaded] = useFonts({
-    SpaceMono: require('../assets/fonts/SpaceMono-Regular.ttf'),
+    SpaceMono: require("../assets/fonts/SpaceMono-Regular.ttf"),
   });
 
   useEffect(() => {
@@ -27,10 +39,47 @@ export default function RootLayout() {
   }
 
   return (
-    <ThemeProvider value={colorScheme === 'dark' ? DarkTheme : DefaultTheme}>
-      <Stack>
-        <Stack.Screen name="(tabs)" options={{ headerShown: false }} />
-      </Stack>
-    </ThemeProvider>
+    <SessionProvider>
+      <ThemeProvider value={colorScheme === "dark" ? DarkTheme : DefaultTheme}>
+        <Tabs
+          screenOptions={{
+            headerShown: false,
+            tabBarShowLabel: false,
+            // tabBarActiveTintColor: Colors.primary,
+          }}
+        >
+          <Tabs.Screen
+            name="index"
+            options={{
+              tabBarIcon: ({ color }) => <HomeIcon color={color} />,
+            }}
+          />
+          <Tabs.Screen
+            name="favorites"
+            options={{
+              tabBarIcon: ({ color }) => <HeartIcon color={color} />,
+            }}
+          />
+          <Tabs.Screen
+            name="search"
+            options={{
+              tabBarIcon: ({ color }) => <SearchIcon color={color} />,
+            }}
+          />
+          <Tabs.Screen
+            name="messages"
+            options={{
+              tabBarIcon: ({ color }) => <MessageCircleIcon color={color} />,
+            }}
+          />
+          <Tabs.Screen
+            name="profile"
+            options={{
+              tabBarIcon: ({ color }) => <UserIcon color={color} />,
+            }}
+          />
+        </Tabs>
+      </ThemeProvider>
+    </SessionProvider>
   );
 }
