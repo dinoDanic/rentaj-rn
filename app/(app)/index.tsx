f89e65721bea01aacef7b392cd186fafd36893ac
Dino Danic <dino.danic@gmail.com>
2024-05-24 21:47:43 +0200

    render info categories

--- app/(app)/index.tsx
+++ app/(app)/index.tsx
@@ -1,29 +1,5 @@
-import { CategoryInfoCard } from "@/features/categories/cards/category-info-card"
-import { useParentCategoriesQuery } from "@/gql/hooks/categories"
-
-import { InfoCardBuilder } from "@/components/ui/cards/info-card-builder"
-import { ContentLayout } from "@/components/ui/content-layout"
-import { Text } from "@/components/ui/text"
+import { RenderInfoCategories } from "@/features/categories/components/render-info-categories"
 
 export default function Index() {
-  return (
-    <ContentLayout title="Kategorije" childrenClassName="-mr-3 -ml-3">
-      <RenderContent />
-    </ContentLayout>
-  )
-}
-
-const RenderContent = () => {
-  const { isLoading, data, error } = useParentCategoriesQuery()
-  if (error) {
-    return <Text>{error.name}</Text>
-  }
-
-  return (
-    <InfoCardBuilder
-      isLoading={isLoading}
-      data={data?.parentCategories}
-      renderItem={(c) => (c.item ? <CategoryInfoCard key={c.item.id} category={c.item} /> : null)}
-    />
-  )
+  return <RenderInfoCategories />
 }
---
+++ features/categories/components/render-info-categories.tsx
@@ -0,0 +1,30 @@
+import { useParentCategoriesQuery } from "@/gql/hooks/categories"
+
+import { InfoCardBuilder } from "@/components/ui/cards/info-card-builder"
+import { ContentLayout } from "@/components/ui/content-layout"
+import { Text } from "@/components/ui/text"
+
+import { CategoryInfoCard } from "../cards/category-info-card"
+
+export const RenderInfoCategories = () => {
+  return (
+    <ContentLayout title="Kategorije" childrenClassName="-mr-3 -ml-3">
+      <RenderContent />
+    </ContentLayout>
+  )
+}
+
+const RenderContent = () => {
+  const { isLoading, data, error } = useParentCategoriesQuery()
+  if (error) {
+    return <Text>{error.name}</Text>
+  }
+
+  return (
+    <InfoCardBuilder
+      isLoading={isLoading}
+      data={data?.parentCategories}
+      renderItem={(c) => (c.item ? <CategoryInfoCard key={c.item.id} category={c.item} /> : null)}
+    />
+  )
+}
