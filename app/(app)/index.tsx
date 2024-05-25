cb6183e8d6e5cd444690642aee5dc209e02fe72f
Dino Danic <dino.danic@gmail.com>
2024-05-25 07:59:56 +0200

    gap 2xl

--- app/(app)/index.tsx
+++ app/(app)/index.tsx
@@ -4,7 +4,7 @@ import { View } from "react-native"
 
 export default function Index() {
   return (
-    <View className="gap-3xl">
+    <View className="gap-2xl">
       <RenderHeroFeature />
       <RenderInfoCategories />
     </View>
