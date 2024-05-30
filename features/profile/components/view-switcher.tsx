0702d13286b9d682d241b78df9b51c28d93f2d68
Dino Danic <dino.danic@gmail.com>
2024-05-30 18:14:04 +0200

    view swithcer

--- features/profile/components/view-switcher.tsx
+++ features/profile/components/view-switcher.tsx
@@ -1,6 +1,7 @@
+import { useState } from "react"
 import { useRentaj } from "@/features/rentaj/hooks/use-rentaj"
 import { RentajView } from "@/features/rentaj/types"
-import { router } from "expo-router"
+import { router, useFocusEffect } from "expo-router"
 import { MotiView } from "moti"
 import { View } from "react-native"
 
@@ -10,6 +11,7 @@ import { Button } from "@/components/ui/button"
 
 export const ViewSwithcer = () => {
   const { view, setView } = useRentaj()
+  const [visible, setVisible] = useState(false)
 
   const switchView = () => {
     const newView: RentajView = view === "explore" ? "rent" : "explore"
@@ -18,15 +20,16 @@ export const ViewSwithcer = () => {
     router.replace(newRoute)
   }
 
+  useFocusEffect(() => {
+    setVisible(true)
+    return () => setVisible(false)
+  })
+
   const title = view === "explore" ? "Prebaci na iznamljivanje" : "Prebaci na istraživanje"
+  const bottom: number = visible ? 0 : -100
   return (
     <View className="absolute bottom-sm w-full">
-      <MotiView
-        from={{ opacity: 0, bottom: -100 }}
-        exit={{ opacity: 0 }}
-        animate={{ opacity: 1, bottom: 0 }}
-        transition={{ type: "spring" }}
-      >
+      <MotiView animate={{ bottom: bottom }} transition={{ type: "spring" }}>
         <Button
           className="self-center"
           size="sm"
