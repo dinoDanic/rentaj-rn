5392520594e86801d06bbbb30adfcddc60d1e88f
Dino Danic <dino.danic@gmail.com>
2024-06-28 08:01:10 +0200

    aj

---
+++ features/item/components/page-by-id/item-page-contact.tsx
@@ -0,0 +1,22 @@
+import { ItemByIdQuery } from "@/gql/generated/graphql"
+import * as Linking from "expo-linking"
+import { View } from "react-native"
+
+import { Button } from "@/components/ui/button"
+
+type Props = ItemByIdQuery
+
+export const ItemPageContact = ({ itemById }: Props) => {
+  if (!itemById) return null
+
+  const call = () => {
+    Linking.openURL(itemById.user.contactNumber)
+  }
+
+  return (
+    <View className="gap-sm">
+      <Button onPress={call} title="Nazovi ogašivača" />
+      <Button variant="secondary" title="Pošalji poruku" />
+    </View>
+  )
+}
